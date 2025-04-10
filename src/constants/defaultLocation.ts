4d3e57e982b0919000246a1a1347e439ce37b4e5
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-04-10 09:24:42 +0500

    Constants updated

---
+++ src/constants/defaultLocation.ts
@@ -0,0 +1,13 @@
+export const DEFAULT_REGION = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
+
+export const LAHORE_PIN = {
+  latitude: 31.5497,
+  longitude: 74.3436,
+  latitudeDelta: 0.2,
+  longitudeDelta: 0.2,
+};
