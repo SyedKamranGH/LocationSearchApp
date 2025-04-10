12bdc4d372f33783f593b5198ba9aec69cbe3b70
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-04-10 08:23:24 +0500

    Created MapView component

---
+++ src/components/MapView/styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    height: '60%',
+    width: '100%',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default styles;
